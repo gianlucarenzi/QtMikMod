0464972ae5dd9e2b589b5bf9d001c859140a5665
Gianluca Renzi <icjtqr@gmail.com>
2026-01-02 09:55:01 +0100

    Add TopazPlus font and set as application font

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/bitmaps/icon.ico"));
+
+    // Load custom font
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/TopazPlus_a500_v1.0.ttf");
+    if (fontId != -1) {
+        QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
+        if (!fontFamilies.isEmpty()) {
+            QFont customFont(fontFamilies.at(0));
+            a.setFont(customFont);
+        }
+    }
     
     MainWindow w;
     w.setWindowTitle("MikMod Player Cross-Platform");
